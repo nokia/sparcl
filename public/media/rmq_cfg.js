7d86daef749cc791d80cf63290b34a7f1ad597a2
soeroesg <gabor.soros@gmail.com>
2022-10-26 18:46:40 +0200

    added dummy rmq_cfg.js

---
+++ public/media/rmq_cfg.js
@@ -0,0 +1,8 @@
+export const config = {
+    rmquser: "",
+    rmqpassword: "",
+    hostname: "",
+    rmqport: 15673, // Secure WebSockets Web-STOMP (wss://)
+    openstreetqueue1: "",
+    penstreetqueue2: ""
+}
