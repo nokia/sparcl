8841b51b17d878bf1ae59ef83a91555538b85fed
soeroesg <gabor.soros@gmail.com>
2024-01-30 15:57:41 +0100

    delete rmq_config.json, rmq_config_gabor.json, snowpack.config.js

--- public/media/rmq_config.js
+++
@@ -1,8 +0,0 @@
-export const rmq_config = {
-    rmquser: "",
-    rmqpassword: "",
-    hostname: "",
-    rmqport: 15673, // Secure WebSockets Web-STOMP (wss://)
-    queue_1: "",
-    queue_2: ""
-}
--- public/media/rmq_config_gabor.js
+++
@@ -1,8 +0,0 @@
-export const rmq_config = {
-    rmquser: "rmquser",
-    rmqpassword: "rmqpassword",
-    hostname: "camloc.xyz",
-    rmqport: 15673, // Secure WebSockets Web-STOMP (wss://)
-    queue_1: "/amq/queue/robot1_queue",
-    queue_2: "/amq/queue/robot2_queue"
-}
